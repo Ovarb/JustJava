f249f808156eeed5f6a2ee978786d7f4ebe8330b
vsevolod.alexeev <vsevolod.alexeev@osinit.com>
2018-06-15 13:16:28 +0300

    name in summary, quantity restrictions + error Toasts

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -10,12 +10,15 @@ package com.example.android.justjava;
 
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 import java.util.jar.Attributes;
@@ -25,7 +28,7 @@ import java.util.jar.Attributes;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 2;
     int price = 0;
 
     @Override
@@ -38,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String customerName = nameField.getText().toString();
+        Log.i("MainActivity", "Customer's name:" + customerName);
+
         CheckBox top01CheckBox = (CheckBox) findViewById(R.id.item01_check_box);
         boolean isCheckedItem01 = top01CheckBox.isChecked();
         Log.i("MainActivity", "Has whipped cream: " + isCheckedItem01);
@@ -46,41 +53,52 @@ public class MainActivity extends AppCompatActivity {
         boolean isCheckedItem02 = top02CheckBox.isChecked();
         Log.i("MainActivity", "Has chocolate: " + isCheckedItem02);
 
-        int price = calculatePrice(isCheckedItem01,isCheckedItem02);
-        String priceMessage = createOrderASummary(price,isCheckedItem01,isCheckedItem02);
+        int price = calculatePrice(isCheckedItem01, isCheckedItem02);
+        String priceMessage = createOrderASummary(customerName, price, isCheckedItem01, isCheckedItem02);
         displayMessage(priceMessage);
     }
 
     /**
      * This method calculates totalPrice
      *
-     * @param isCheckedOption01 is about added topping01
-     * @param isCheckedOption02 is about added topping02
+     *
+     * @param isCheckedOption01 whether or not topping01 is added
+     * @param isCheckedOption02 whether or not topping01 is added
      * @return totalPrice of the order
      */
     public int calculatePrice(boolean isCheckedOption01, boolean isCheckedOption02){
+
+        int basePrice;
         int totalPrice;
-        totalPrice = quantity * 5;
+
+        basePrice = 5;
         if (isCheckedOption01) {
-            totalPrice += 10;
+            basePrice += 1;
         }
+
         if (isCheckedOption02) {
-            totalPrice += 12;
+            basePrice += 2;
         }
-        Log.i("MainActivity", "The price is: " + totalPrice);
+
+        Log.i("MainActivity", "The base price of single cup: " + basePrice);
+
+        totalPrice = quantity * basePrice;
+        Log.i("MainActivity", "The total price is: " + totalPrice);
+
         return totalPrice;
     }
 
     /**
      * Create summary of the order
      *
+     *
+     * @param customerName of the buyer
      * @param price of the order
-     * @param isCheckedOption01 is about added topping01
-     * @param isCheckedOption02 is about added topping02
+     * @param isCheckedOption01 whether or not topping01 is added
+     * @param isCheckedOption02 whether or not topping02 is added
      * @return text summary about the order
      */
-    private String createOrderASummary(int price, boolean isCheckedOption01, boolean isCheckedOption02){
-        String customerName = "Kaptain Kunal";
+    private String createOrderASummary(String customerName, int price, boolean isCheckedOption01, boolean isCheckedOption02){
         String message;
         message = "Name: " + customerName;
         message += "\n" + "Add whipped cream? " + isCheckedOption01;
@@ -95,6 +113,18 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the + button is clicked.
      */
     public void increment (View view) {
+        if(10 == quantity) {
+            //shows an error message
+            Context context = getApplicationContext();
+            String toastText = "You can't order more than 10 cup of coffee";
+            int toastDuration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, toastText, toastDuration);
+            toast.show();
+
+            //return from the method if quantity is too small
+            return;
+        }
         quantity++;
         display(quantity);
         //displayPrice(numberOfCoffees*5);
@@ -104,6 +134,12 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the - button is clicked.
      */
     public void decrement (View view) {
+        if(1 == quantity ) {
+            //shows an error message
+            Toast.makeText(this, "You can't order less than 1 cup of coffee", Toast.LENGTH_SHORT).show();
+            //return from the method if quantity is too small
+            return;
+        }
         quantity--;
         display(quantity);
         //displayPrice(numberOfCoffees*5);
